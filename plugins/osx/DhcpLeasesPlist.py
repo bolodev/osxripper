e6513df630f8d69df1c87df0fa9af965951dfec7
bolodev <bolodevel@gmail.com>
2020-07-18 12:34:56 +0100

    Refactor for pylint

--- plugins/osx/DhcpLeasesPlist.py
+++ plugins/osx/DhcpLeasesPlist.py
@@ -1,9 +1,11 @@
-from riplib.plugin import Plugin
+""" Module to parse DHCP leases """
 import binascii
 import codecs
 import logging
 import os
 import plistlib
+from riplib.plugin import Plugin
+
 
 __author__ = 'osxripper'
 __version__ = '0.1'
@@ -14,7 +16,7 @@ class DhcpLeasesPlist(Plugin):
     """
     Plugin to parse DHCP plists in /private/var/db/dhcpclient/leases/en
     """
-    
+
     def __init__(self):
         """
         Initialise plugins
@@ -25,7 +27,7 @@ class DhcpLeasesPlist(Plugin):
         self._data_file = ""  # Empty as parsing multiple files
         self._output_file = "Networking.txt"
         self._type = "plist"
-        
+
     def parse(self):
         """
         Parse DHCP plists in /private/var/db/dhcpclient/leases/en
@@ -33,66 +35,93 @@ class DhcpLeasesPlist(Plugin):
         working_dir = os.path.join(self._input_dir, "private", "var", "db", "dhcpclient", "leases")
         if os.path.isdir(working_dir):
             file_listing = os.listdir(working_dir)
-            for f in file_listing:
-                self.__parse_plist(os.path.join(working_dir, f))
+            for file_name in file_listing:
+                self.__parse_plist(os.path.join(working_dir, file_name))
         else:
-            with codecs.open(os.path.join(self._output_dir, self._output_file), "a", encoding="utf-8") as of:
-                of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
-                of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(working_dir))
-                of.write("="*40 + " " + "\r\n\r\n")
-            of.close()
-            logging.warning("File: {0} does not exist or cannot be found.".format(working_dir))
+            with codecs.open(os.path.join(self._output_dir, self._output_file), "a", encoding="utf-8") as output_file:
+                output_file.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
+                output_file.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(working_dir))
+                output_file.write("="*40 + " " + "\r\n\r\n")
+            output_file.close()
+            logging.warning("File: %s does not exist or cannot be found.", working_dir)
             print("[WARNING] File: {0} does not exist or cannot be found.".format(working_dir))
-    
+
     def __parse_plist(self, file):
         """
         Parse the plist
         """
-        with codecs.open(os.path.join(self._output_dir, self._output_file), "a", encoding="utf-8") as of:
-            of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
-            of.write("Source File: {0}\r\n\r\n".format(file))
-            # TODO investigate packet data bytes for useful information
+        with codecs.open(os.path.join(self._output_dir, self._output_file), "a", encoding="utf-8") as output_file:
+            output_file.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
+            output_file.write("Source File: {0}\r\n\r\n".format(file))
+            with open(file, "rb") as plist_to_load:
+                plist = plistlib.load(plist_to_load)
+            plist_to_load.close()
+            # Investigate packet data bytes for useful information
             # if self._os_version in ["big_sur", "catalina", "mojave", "high_sierra", "sierra", "el_capitan"]:
             if self._os_version in ["catalina", "mojave", "high_sierra", "sierra", "el_capitan"]:
-                try:
-                    with open(file, "rb") as pl:
-                        plist = plistlib.load(pl)
-                    if "IPAddress" in plist:
-                        of.write("IP Address             : {0}\r\n".format(plist["IPAddress"]))
-                    if "LeaseLength" in plist:
-                        of.write("Lease Length           : {0}\r\n".format(plist["LeaseLength"]))
-                    if "LeaseStartDate" in plist:
-                        of.write("Lease Start Date       : {0}\r\n".format(plist["LeaseStartDate"]))
-                    if "RouterHardwareAddress" in plist:
-                        # BASE64 is MAC in raw
-                        of.write("Router Hardware Address: {0}\r\n"
-                                 .format(binascii.hexlify(plist["RouterHardwareAddress"])))
-
-                    if "RouterIPAddress" in plist:
-                        of.write("Router IP Address      : {0}\r\n".format(plist["RouterIPAddress"]))
-                    if "SSID" in plist:
-                        of.write("Router SSID            : {0}\r\n".format(plist["SSID"]))
-                except KeyError:
-                    pass
+                parse_os = Parse01(output_file, plist)
+                parse_os.parse()
             elif self._os_version in ["yosemite", "mavericks", "mountain_lion", "lion", "snow_leopard"]:
-                try:
-                    with open(file, "rb") as pl:
-                        plist = plistlib.load(pl)
-                    if "IPAddress" in plist:
-                        of.write("IP Address: {0}\r\n".format(plist["IPAddress"]))
-                    if "LeaseLength" in plist:
-                        of.write("Lease Length: {0}\r\n".format(plist["LeaseLength"]))
-                    if "LeaseStartDate" in plist:
-                        of.write("Lease Start Date: {0}\r\n".format(plist["LeaseStartDate"]))
-                    if "RouterHardwareAddress" in plist:
-                        of.write("Router Hardware Address: {0}\r\n"
-                                 .format(binascii.hexlify(plist["RouterHardwareAddress"])))
-                    if "RouterIPAddress" in plist:
-                        of.write("Router IP Address: {0}\r\n".format(plist["RouterIPAddress"]))
-                except KeyError:
-                    pass
+                parse_os = Parse02(output_file, plist)
+                parse_os.parse()
             else:
                 logging.warning("Not a known OSX version.")
                 print("[WARNING] Not a known OSX version.")
-            of.write("="*40 + "\r\n\r\n")
-        of.close()
+            output_file.write("="*40 + "\r\n\r\n")
+        output_file.close()
+
+class Parse01():
+    """
+    Convenience class for parsing macOS data
+    """
+    def __init__(self, output_file, data_file):
+        self._output_file = output_file
+        self._data_file = data_file
+
+    def parse(self):
+        """
+        Parse data
+        """
+        try:
+            if "IPAddress" in self._data_file:
+                self._output_file.write("IP Address             : {0}\r\n".format(self._data_file["IPAddress"]))
+            if "LeaseLength" in self._data_file:
+                self._output_file.write("Lease Length           : {0}\r\n".format(self._data_file["LeaseLength"]))
+            if "LeaseStartDate" in self._data_file:
+                self._output_file.write("Lease Start Date       : {0}\r\n".format(self._data_file["LeaseStartDate"]))
+            if "RouterHardwareAddress" in self._data_file:
+                # BASE64 is MAC in raw
+                self._output_file.write("Router Hardware Address: {0}\r\n".format(binascii.hexlify(self._data_file["RouterHardwareAddress"])))
+            if "RouterIPAddress" in self._data_file:
+                self._output_file.write("Router IP Address      : {0}\r\n".format(self._data_file["RouterIPAddress"]))
+            if "SSID" in self._data_file:
+                self._output_file.write("Router SSID            : {0}\r\n".format(self._data_file["SSID"]))
+        except KeyError:
+            pass
+
+
+class Parse02():
+    """
+    Convenience class for parsing macOS data
+    """
+    def __init__(self, output_file, data_file):
+        self._output_file = output_file
+        self._data_file = data_file
+
+    def parse(self):
+        """
+        Parse data
+        """
+        try:
+            if "IPAddress" in self._data_file:
+                self._output_file.write("IP Address: {0}\r\n".format(self._data_file["IPAddress"]))
+            if "LeaseLength" in self._data_file:
+                self._output_file.write("Lease Length: {0}\r\n".format(self._data_file["LeaseLength"]))
+            if "LeaseStartDate" in self._data_file:
+                self._output_file.write("Lease Start Date: {0}\r\n".format(self._data_file["LeaseStartDate"]))
+            if "RouterHardwareAddress" in self._data_file:
+                self._output_file.write("Router Hardware Address: {0}\r\n".format(binascii.hexlify(self._data_file["RouterHardwareAddress"])))
+            if "RouterIPAddress" in self._data_file:
+                self._output_file.write("Router IP Address: {0}\r\n".format(self._data_file["RouterIPAddress"]))
+        except KeyError:
+            pass
