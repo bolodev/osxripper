3cdbd24ff8a38a895725d42196b81225cae6725a
bolodev <bolodevel@gmail.com>
2020-07-21 22:11:12 +0100

    Refactor for pylint

--- plugins/osx/SystemKnowledgeC.py
+++ plugins/osx/SystemKnowledgeC.py
@@ -1,8 +1,10 @@
-from riplib.plugin import Plugin
+""" Module to information from /private/var/db/CoreDuet/Knowledge/KnowledgeC.db reference """
 import codecs
 import logging
 import os
 import sqlite3
+from riplib.plugin import Plugin
+
 
 __author__ = 'osxripper'
 __version__ = '0.1'
@@ -19,11 +21,11 @@ class SystemKnowledgeC(Plugin):
         Initialise the class.
         """
         super().__init__()
-        self._name = "System KnowledgeC"
-        self._description = "Parse information from /private/var/db/CoreDuet/Knowledge/KnowledgeC.db"
-        self._data_file = "KnowledgeC.db"
-        self._output_file = "System_KnowledgeC.txt"
-        self._type = "sqlite"
+        self.set_name("System KnowledgeC")
+        self.set_description("Parse information from /private/var/db/CoreDuet/Knowledge/KnowledgeC.db")
+        self.set_data_file("KnowledgeC.db")
+        self.set_output_file("System_KnowledgeC.txt")
+        self.set_type("sqlite")
 
     def parse(self):
         """
@@ -53,14 +55,13 @@ class SystemKnowledgeC(Plugin):
 
         headers = "ENTRY CREATION\tDAY OF WEEK\tGMT OFFSET\tSTART\tEND\tUSAGE IN SECONDS\tSTREAMNAME\tVALUESTRING\r\n"
 
-        with codecs.open(os.path.join(self._output_dir, self._output_file), "a", encoding="utf-8") as of:
-            of.write("=" * 10 + " " + self._name + " " + "=" * 10 + "\r\n")
+        with codecs.open(os.path.join(self._output_dir, self._output_file), "a", encoding="utf-8") as output_file:
+            output_file.write("=" * 10 + " " + self._name + " " + "=" * 10 + "\r\n")
             file = os.path.join(self._input_dir, "private", "var", "db", "CoreDuet", "Knowledge", self._data_file)
-            of.write("Source File: {0}\r\n\r\n".format(file))
+            output_file.write("Source File: {0}\r\n\r\n".format(file))
             # if self._os_version in ["big_sur", "catalina", "mojave", "high_sierra"]:
             if self._os_version in ["catalina", "mojave", "high_sierra"]:
                 if os.path.isfile(file):
-                    pass
                     conn = None
                     try:
                         sqlite_connection = sqlite3.connect(file)
@@ -69,36 +70,36 @@ class SystemKnowledgeC(Plugin):
                             cur = sqlite_connection.cursor()
                             cur.execute(mac4n6_sql)
                             rows = cur.fetchall()
-                            of.write(headers)
+                            output_file.write(headers)
                             for row in rows:
-                                of.write("{0}\t".format(row["ENTRY CREATION"]))
-                                of.write("{0}\t".format(row["DAY OF WEEK"]))
-                                of.write("{0}\t".format(row["GMT OFFSET"]))
-                                of.write("{0}\t".format(row["START"]))
-                                of.write("{0}\t".format(row["END"]))
-                                of.write("{0}\t".format(row["USAGE IN SECONDS"]))
-                                of.write("{0}\t".format(row["ZSTREAMNAME"]))
-                                of.write("{0}\t".format(row["ZVALUESTRING"]))
-                                of.write("\r\n")
-                        of.write("\r\n")
-                    except sqlite3.Error as e:
-                        logging.error("{0}".format(e.args[0]))
-                        print("[ERROR] {0}".format(e.args[0]))
+                                output_file.write("{0}\t".format(row["ENTRY CREATION"]))
+                                output_file.write("{0}\t".format(row["DAY OF WEEK"]))
+                                output_file.write("{0}\t".format(row["GMT OFFSET"]))
+                                output_file.write("{0}\t".format(row["START"]))
+                                output_file.write("{0}\t".format(row["END"]))
+                                output_file.write("{0}\t".format(row["USAGE IN SECONDS"]))
+                                output_file.write("{0}\t".format(row["ZSTREAMNAME"]))
+                                output_file.write("{0}\t".format(row["ZVALUESTRING"]))
+                                output_file.write("\r\n")
+                        output_file.write("\r\n")
+                    except sqlite3.Error as error:
+                        logging.error("%s", error.args[0])
+                        print("[ERROR] {0}".format(error.args[0]))
                     finally:
                         if conn:
                             conn.close()
                 else:
-                    logging.warning("File: {0} does not exist or cannot be found.\r\n".format(file))
-                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+                    logging.warning("File: %s does not exist or cannot be found.\r\n", file)
+                    output_file.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
                     print("[WARNING] File: {0} does not exist or cannot be found.".format(file))
 
             elif self._os_version in ["yosemite", "mavericks", "mountain_lion", "lion", "snow_leopard", "sierra",
                                       "el_capitan"]:
                 logging.info("This version of OSX is not supported by this plugin.")
                 print("[INFO] This version of OSX is not supported by this plugin.")
-                of.write("[INFO] This version of OSX is not supported by this plugin.\r\n")
+                output_file.write("[INFO] This version of OSX is not supported by this plugin.\r\n")
             else:
                 logging.warning("Not a known OSX version.")
                 print("[WARNING] Not a known OSX version.")
-            of.write("=" * 40 + "\r\n\r\n")
-        of.close()
+            output_file.write("=" * 40 + "\r\n\r\n")
+        output_file.close()
--- plugins/osx/SystemLaunchdDisabled.py
+++ plugins/osx/SystemLaunchdDisabled.py
@@ -1,8 +1,10 @@
-from riplib.plugin import Plugin
+""" Module to retrieve information from /private/var/db/com.apple.xpc.launchd/disabled.plist """
 import codecs
 import logging
 import os
 import plistlib
+from riplib.plugin import Plugin
+
 
 __author__ = 'osxripper'
 __version__ = '0.1'
@@ -18,41 +20,40 @@ class SystemLaunchdDisabled(Plugin):
         Initialise the class.
         """
         super().__init__()
-        self._name = "System Launchd Disabled"
-        self._description = "Get the information from /private/var/db/com.apple.xpc.launchd/disabled.plist"
-        self._data_file = "disabled.plist"
-        self._output_file = "System_Launch.txt"
-        self._type = "plist"
-        
-    def parse(self): 
+        self.set_name("System Launchd Disabled")
+        self.set_description("Get the information from /private/var/db/com.apple.xpc.launchd/disabled.plist")
+        self.set_data_file("disabled.plist")
+        self.set_output_file("System_Launch.txt")
+        self.set_type("plist")
+
+    def parse(self):
         """
         Parse SystemVersion.plist and write version information to file
         """
-        with codecs.open(os.path.join(self._output_dir, self._output_file), "a", encoding="utf-8") as of:
-            of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
+        with codecs.open(os.path.join(self._output_dir, self._output_file), "a", encoding="utf-8") as output_file:
+            output_file.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
             plist_file = os.path.join(self._input_dir, "private", "var", "db", "com.apple.xpc.launchd", self._data_file)
-            of.write("Source File: {0}\r\n\r\n".format(plist_file))
+            output_file.write("Source File: {0}\r\n\r\n".format(plist_file))
             # if self._os_version in ["big_sur", "catalina", "mojave", "high_sierra", "sierra", "el_capitan", "yosemite"]:
             if self._os_version in ["catalina", "mojave", "high_sierra", "sierra", "el_capitan", "yosemite"]:
                 if os.path.isfile(plist_file):
                     try:
-                        with open(plist_file, "rb") as pl:
-                            plist = plistlib.load(pl)
+                        with open(plist_file, "rb") as plist_to_load:
+                            plist = plistlib.load(plist_to_load)
                         for launchd_item in plist:
-                            of.write("{0}: {1}\r\n".format(launchd_item, plist[launchd_item]))
-
+                            output_file.write("{0}: {1}\r\n".format(launchd_item, plist[launchd_item]))
                     except KeyError:
                         pass
                 else:
-                    logging.warning("File: {0} does not exist or cannot be found.\r\n".format(plist_file))
-                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(plist_file))
+                    logging.warning("File: %s does not exist or cannot be found.\r\n", plist_file)
+                    output_file.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(plist_file))
                     print("[WARNING] File: {0} does not exist or cannot be found.".format(plist_file))
             elif self._os_version in ["mavericks", "mountain_lion", "lion", "snow_leopard"]:
                 logging.info("This version of OSX is not supported by this plugin.")
                 print("[INFO] This version of OSX is not supported by this plugin.")
-                of.write("[INFO] This version of OSX is not supported by this plugin.\r\n")
+                output_file.write("[INFO] This version of OSX is not supported by this plugin.\r\n")
             else:
                 logging.warning("Not a known OSX version.")
                 print("[WARNING] Not a known OSX version.")
-            of.write("="*40 + "\r\n\r\n")
-        of.close()
+            output_file.write("="*40 + "\r\n\r\n")
+        output_file.close()
