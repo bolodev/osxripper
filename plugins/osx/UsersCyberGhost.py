9fbbafc8ecec8fc0b4473c5fc9cfad9f5511a971
bolodev <bolodevel@gmail.com>
2020-07-25 00:01:32 +0100

    Refactor for pylint

--- plugins/osx/UsersCyberGhost.py
+++ plugins/osx/UsersCyberGhost.py
@@ -1,9 +1,11 @@
-from riplib.plugin import Plugin
-import riplib.ccl_bplist
+""" Mdule to parse CyberGhost plist """
 import codecs
 import datetime
 import logging
 import os
+import riplib.ccl_bplist
+from riplib.plugin import Plugin
+
 
 __author__ = 'bolodev'
 __version__ = '0.1'
@@ -20,13 +22,12 @@ class UsersCyberGhost(Plugin):
         Initialise the class.
         """
         super().__init__()
-        self._name = "User Cyber Ghost VPN Configuration"
-        self._description = "Parse information from " \
-                            "/Users/{username}/Library/Preferences/com.cyberghostsrl.cyberghostmac.plist"
-        self._data_file = "com.cyberghostsrl.cyberghostmac.plist"
-        self._output_file = ""  # this will have to be defined per user account
-        self._type = "bplist"
-    
+        self.set_name("User Cyber Ghost VPN Configuration")
+        self.set_description("Parse information from /Users/{username}/Library/Preferences/com.cyberghostsrl.cyberghostmac.plist")
+        self.set_data_file("com.cyberghostsrl.cyberghostmac.plist")
+        self.set_output_file("")  # this will have to be defined per user account
+        self.set_type("bplist")
+
     def parse(self):
         """
         Scan for the plist
@@ -40,53 +41,52 @@ class UsersCyberGhost(Plugin):
                     if os.path.isfile(config):
                         self.__parse_plist(config, username)
                     else:
-                        logging.warning("{0} does not exist.".format(config))
+                        logging.warning("%s does not exist.", config)
                         print("[WARNING] {0} does not exist.".format(config))
         else:
             print("[WARNING] {0} does not exist.".format(users_path))
-            
+
     def __parse_plist(self, file, username):
         """
         Parse /Users/{username}/Library/Preferences/com.cyberghostsrl.cyberghostmac.plist
         """
-        with codecs.open(os.path.join(self._output_dir, "Users_" + username + "_VPN_CyberGhost.txt"), "a",
-                         encoding="utf-8") as of:
-            of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
-            of.write("Source File: {0}\r\n\r\n".format(file))
+        with codecs.open(os.path.join(self._output_dir, "Users_" + username + "_VPN_CyberGhost.txt"), "a", encoding="utf-8") as output_file:
+            output_file.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
+            output_file.write("Source File: {0}\r\n\r\n".format(file))
             if os.path.isfile(file):
                 bplist = open(file, "rb")
-                pl = riplib.ccl_bplist.load(bplist)
+                plist = riplib.ccl_bplist.load(bplist)
                 bplist.close()
                 try:
-                    if "Cyberghost_GUID" in pl:
-                        of.write("Cyberghost GUID      : {0}\r\n".format(pl["Cyberghost_GUID"]))
-                    if "startAtSystemStart" in pl:
-                        of.write("Start at System Start: {0}\r\n".format(pl["startAtSystemStart"]))
-                    if "SULastCheckTime" in pl:
-                        of.write("SU Last Check Time   : {0}\r\n".format(pl["SULastCheckTime"]))
-                    if "RandomPort" in pl:
-                        of.write("Random Port          : {0}\r\n".format(pl["RandomPort"]))
-                    if "useOpenVpnOverTcp" in pl:
-                        of.write("Use OpenVpn Over Tcp : {0}\r\n".format(pl["useOpenVpnOverTcp"]))
-                    if "Cyberghost_RemoteSettings" in pl:
-                        of.write("Cyberghost Remote Settings:\r\n")
-                        remote_settings = pl["Cyberghost_RemoteSettings"]
+                    if "Cyberghost_GUID" in plist:
+                        output_file.write("Cyberghost GUID      : {0}\r\n".format(plist["Cyberghost_GUID"]))
+                    if "startAtSystemStart" in plist:
+                        output_file.write("Start at System Start: {0}\r\n".format(plist["startAtSystemStart"]))
+                    if "SULastCheckTime" in plist:
+                        output_file.write("SU Last Check Time   : {0}\r\n".format(plist["SULastCheckTime"]))
+                    if "RandomPort" in plist:
+                        output_file.write("Random Port          : {0}\r\n".format(plist["RandomPort"]))
+                    if "useOpenVpnOverTcp" in plist:
+                        output_file.write("Use OpenVpn Over Tcp : {0}\r\n".format(plist["useOpenVpnOverTcp"]))
+                    if "Cyberghost_RemoteSettings" in plist:
+                        output_file.write("Cyberghost Remote Settings:\r\n")
+                        remote_settings = plist["Cyberghost_RemoteSettings"]
                         # instdate -> Unix Millisecond
                         date_foo = remote_settings["instdate"]
                         date_foo = datetime.datetime.fromtimestamp(date_foo/1000.0)
-                        of.write("\tInstall Date           : {0}\r\n".format(date_foo))
-                        of.write("\tLast Country           : {0}\r\n".format(remote_settings["LastCountry"]))
-                        of.write("\tLast Server            : {0}\r\n".format(remote_settings["LastServer"]))
-                        of.write("\tLast Logged In         : {0}\r\n".format(remote_settings["LastLoggedIn"]))
-                        of.write("\tLas tPlan ID           : {0}\r\n".format(remote_settings["LastPlanID"]))
-                        of.write("\tENC Connected Server ID: {0}\r\n".format(remote_settings["ENC_ConnectedServerID"]))
-                        of.write("\tLast Plan Name         : {0}\r\n".format(remote_settings["LastPlanName"]))
-                        of.write("\tStart Counter          : {0}\r\n".format(remote_settings["startCounter"]))
+                        output_file.write("\tInstall Date           : {0}\r\n".format(date_foo))
+                        output_file.write("\tLast Country           : {0}\r\n".format(remote_settings["LastCountry"]))
+                        output_file.write("\tLast Server            : {0}\r\n".format(remote_settings["LastServer"]))
+                        output_file.write("\tLast Logged In         : {0}\r\n".format(remote_settings["LastLoggedIn"]))
+                        output_file.write("\tLas tPlan ID           : {0}\r\n".format(remote_settings["LastPlanID"]))
+                        output_file.write("\tENC Connected Server ID: {0}\r\n".format(remote_settings["ENC_ConnectedServerID"]))
+                        output_file.write("\tLast Plan Name         : {0}\r\n".format(remote_settings["LastPlanName"]))
+                        output_file.write("\tStart Counter          : {0}\r\n".format(remote_settings["startCounter"]))
                 except KeyError:
                     pass
             else:
-                logging.warning("File: {0} does not exist or cannot be found.\r\n".format(file))
-                of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+                logging.warning("File: %s does not exist or cannot be found.\r\n", file)
+                output_file.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
                 print("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
-            of.write("="*40 + "\r\n\r\n")
-        of.close()
+            output_file.write("="*40 + "\r\n\r\n")
+        output_file.close()
--- plugins/osx/UsersCyberGhostLogs.py
+++ plugins/osx/UsersCyberGhostLogs.py
@@ -1,7 +1,8 @@
-from riplib.plugin import Plugin
+""" Module to parse information from CyberGhost log """
 import codecs
 import logging
 import os
+from riplib.plugin import Plugin
 
 __author__ = 'osxripper'
 __version__ = '0.1'
@@ -18,12 +19,12 @@ class UsersCyberGhostLogs(Plugin):
         Initialise the class.
         """
         super().__init__()
-        self._name = "User Cyber Ghost VPN Logs"
-        self._description = "Parse information from /Users/{username}/Library/Application Support/CyberGhost {version}"
-        self._data_file = ""
-        self._output_file = ""  # this will have to be defined per user account
-        self._type = "multiple"
-    
+        self.set_name("User Cyber Ghost VPN Logs")
+        self.set_description("Parse information from /Users/{username}/Library/Application Support/CyberGhost {version}")
+        self.set_data_file("")
+        self.set_output_file("")  # this will have to be defined per user account
+        self.set_type("multiple")
+
     def parse(self):
         """
         Scan for the plist
@@ -37,38 +38,37 @@ class UsersCyberGhostLogs(Plugin):
                     if os.path.isdir(config):
                         self.__read_logs(config, username)
                     else:
-                        logging.warning("{0} does not exist.".format(config))
+                        logging.warning("%s does not exist.", config)
                         print("[WARNING] {0} does not exist.".format(config))
         else:
             print("[WARNING] {0} does not exist.".format(users_path))
-            
+
     def __read_logs(self, file, username):
         """
         Parse /Users/{username}/Library/Application Support/CyberGhost {version}
         """
-        with codecs.open(os.path.join(self._output_dir, "Users_" + username + "_VPN_CyberGhost.txt"), "a",
-                         encoding="utf-8") as of:
-            of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
+        with codecs.open(os.path.join(self._output_dir, "Users_" + username + "_VPN_CyberGhost.txt"), "a", encoding="utf-8") as output_file:
+            output_file.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
             app_support_dir = os.listdir(file)
             for directory in app_support_dir:
                 if "CyberGhost" in directory:
                     ghost_dir = os.path.join(file, directory)
-                    of.write("Source Directory: {0}\r\n\r\n".format(ghost_dir))
+                    output_file.write("Source Directory: {0}\r\n\r\n".format(ghost_dir))
                     ghost_dir_list = os.listdir(ghost_dir)
                     for ghost_file in ghost_dir_list:
                         if ghost_file == "CyberGhostMacLog.log":
-                            of.write("="*10 + " " + ghost_file + " " + "="*10 + "\r\n")
+                            output_file.write("="*10 + " " + ghost_file + " " + "="*10 + "\r\n")
                             with open(os.path.join(ghost_dir, ghost_file), "r") as mac_log:
                                 for line in mac_log.readlines():
-                                    of.write("{0}\r\n".format(line))
+                                    output_file.write("{0}\r\n".format(line))
                             mac_log.close()
-                            of.write("\r\n")
+                            output_file.write("\r\n")
                         if ghost_file == "CyberGhostMacLogScripts.log":
-                            of.write("="*10 + " " + ghost_file + " " + "="*10 + "\r\n")
+                            output_file.write("="*10 + " " + ghost_file + " " + "="*10 + "\r\n")
                             with open(os.path.join(ghost_dir, ghost_file), "r") as scripts_log:
                                 for line in scripts_log.readlines():
-                                    of.write("{0}\r\n".format(line))
+                                    output_file.write("{0}\r\n".format(line))
                             scripts_log.close()
-                            of.write("\r\n")
-            of.write("="*40 + "\r\n\r\n")
-        of.close()
+                            output_file.write("\r\n")
+            output_file.write("="*40 + "\r\n\r\n")
+        output_file.close()
