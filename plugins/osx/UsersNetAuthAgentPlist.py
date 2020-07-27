7b8d2faf4c2e411993e36b70a2d9b29771d46f81
bolodev <bolodevel@gmail.com>
2020-07-27 17:51:05 +0100

    Refactor for pylint

--- plugins/osx/UsersNetAuthAgentPlist.py
+++ plugins/osx/UsersNetAuthAgentPlist.py
@@ -1,8 +1,10 @@
-from riplib.plugin import Plugin
+""" Module to parse information form NetAuthAgent plist """
 import codecs
 import logging
 import os
 import riplib.ccl_bplist
+from riplib.plugin import Plugin
+
 
 __author__ = 'osxripper'
 __version__ = '0.1'
@@ -19,11 +21,11 @@ class UsersNetAuthAgentPlist(Plugin):
         Initialise the class.
         """
         super().__init__()
-        self._name = "User Mounted Shares"
-        self._description = "Parse information from /Users/username/Library/Preferences/com.apple.NetAuthAgent.plist"
-        self._data_file = "com.apple.NetAuthAgent.plist"
-        self._output_file = ""  # this will have to be defined per user account
-        self._type = "bplist"
+        self.set_name("User Mounted Shares")
+        self.set_description("Parse information from /Users/username/Library/Preferences/com.apple.NetAuthAgent.plist")
+        self.set_data_file("com.apple.NetAuthAgent.plist")
+        self.set_output_file("")  # this will have to be defined per user account
+        self.set_type("bplist")
 
     def parse(self):
         """
@@ -38,47 +40,46 @@ class UsersNetAuthAgentPlist(Plugin):
                     if os.path.isfile(plist):
                         self.__parse_bplist(plist, username)
                     else:
-                        logging.warning("{0} does not exist.".format(plist))
+                        logging.warning("%s does not exist.", plist)
                         print("[WARNING] {0} does not exist.".format(plist))
         else:
-            logging.warning("{0} does not exist.".format(users_path))
+            logging.warning("%s does not exist.", users_path)
             print("[WARNING] {0} does not exist.".format(users_path))
 
     def __parse_bplist(self, file, username):
         """
         Parse /Users/username/Library/Preferences/com.apple.finder.plist
         """
-        with codecs.open(os.path.join(self._output_dir, "Users_" + username + "_Shares.txt"), "a",
-                         encoding="utf-8") as of:
-            of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
-            of.write("Source File: {0}\r\n\r\n".format(file))
+        with codecs.open(os.path.join(self._output_dir, "Users_" + username + "_Shares.txt"), "a", encoding="utf-8") as output_file:
+            output_file.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
+            output_file.write("Source File: {0}\r\n\r\n".format(file))
             # if self._os_version in ["big_sur", "catalina", "mojave", "high_sierra", "sierra", "el_capitan"]:
             if self._os_version in ["catalina", "mojave", "high_sierra", "sierra", "el_capitan"]:
                 if os.path.isfile(file):
                     bplist = open(file, "rb")
                     plist = riplib.ccl_bplist.load(bplist)
-                    try:
-                        if "PreviouslySelectedShares" in plist:
-                            for key in plist["PreviouslySelectedShares"].keys():
-                                of.write("Host: {0}\r\n".format(key))
-                                for directory in plist["PreviouslySelectedShares"][key]:
-                                    of.write("\tShared Directory: {0}\r\n".format(directory))
-                                of.write("\r\n")
-                        of.write("\r\n")
-                    except KeyError:
-                        pass
                     bplist.close()
                 else:
-                    logging.warning("File: {0} does not exist or cannot be found.".format(file))
-                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+                    logging.warning("File: %s does not exist or cannot be found.", file)
+                    output_file.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
                     print("[WARNING] File: {0} does not exist or cannot be found.".format(file))
-
+                    return
+                try:
+                    if "PreviouslySelectedShares" in plist:
+                        for key in plist["PreviouslySelectedShares"].keys():
+                            output_file.write("Host: {0}\r\n".format(key))
+                            for directory in plist["PreviouslySelectedShares"][key]:
+                                output_file.write("\tShared Directory: {0}\r\n".format(directory))
+                            output_file.write("\r\n")
+                    output_file.write("\r\n")
+                except KeyError:
+                    pass
             elif self._os_version in ["yosemite", "mavericks", "mountain_lion", "lion", "snow_leopard"]:
-                    logging.warning("File: {0} does not exist on this version of OSX.".format(file))
-                    of.write("[WARNING] File: {0} does not exist on this version of OSX.\r\n".format(file))
-                    print("[WARNING] File: {0} does not exist on this version of OSX.".format(file))
+                logging.warning("File: %s does not exist on this version of OSX.", file)
+                output_file.write("[WARNING] File: {0} does not exist on this version of OSX.\r\n".format(file))
+                print("[WARNING] File: {0} does not exist on this version of OSX.".format(file))
             else:
                 logging.warning("Not a known OSX version.")
                 print("[WARNING] Not a known OSX version.")
-            of.write("="*40 + "\r\n\r\n")
-        of.close()
+            output_file.write("="*40 + "\r\n\r\n")
+        output_file.close()
--- plugins/osx/UsersQuarantineEventsV2.py
+++ plugins/osx/UsersQuarantineEventsV2.py
@@ -1,10 +1,11 @@
-from riplib.plugin import Plugin
+""" Module to parse QuarantineEventsV2 database """
 import codecs
-import datetime
+# import datetime
 import logging
 import os
-import riplib.osxripper_time
 import sqlite3
+import riplib.osxripper_time
+from riplib.plugin import Plugin
 
 __author__ = 'osxripper'
 __version__ = '0.1'
@@ -21,13 +22,12 @@ class UsersQuarantineEventsV2(Plugin):
         Initialise the class.
         """
         super().__init__()
-        self._name = "User LaunchServices.QuarantineEventsV2"
-        self._description = "Parse information from " \
-                            "/Users/<username>/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV2"
-        self._data_file = "com.apple.LaunchServices.QuarantineEventsV2"
-        self._output_file = ""  # this will have to be defined per user account
-        self._type = "sqlite"
-    
+        self.set_name("User LaunchServices.QuarantineEventsV2")
+        self.set_description("Parse information from /Users/<username>/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV2")
+        self.set_data_file("com.apple.LaunchServices.QuarantineEventsV2")
+        self.set_output_file("")  # this will have to be defined per user account
+        self.set_type("sqlite")
+
     def parse(self):
         """
         Iterate over /Users directory and find user sub-directories
@@ -46,19 +46,18 @@ class UsersQuarantineEventsV2(Plugin):
                     if os.path.isfile(sqlite_db):
                         self.__parse_sqlite_db(sqlite_db, username)
                     else:
-                        logging.warning("{0} does not exist.".format(sqlite_db))
+                        logging.warning("%s does not exist.", sqlite_db)
                         print("[WARNING] {0} does not exist.".format(sqlite_db))
         else:
-            logging.warning("{0} does not exist.".format(users_path))
+            logging.warning("%s does not exist.", users_path)
             print("[WARNING] {0} does not exist.".format(users_path))
-    
+
     def __parse_sqlite_db(self, file, username):
         """
         Read the com.apple.LaunchServices.QuarantineEventsV2 SQLite database
         """
-        with codecs.open(os.path.join(self._output_dir, "Users_" + username + "_Quarantine_Events.txt"), "a",
-                         encoding="utf-8") as of:
-            of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
+        with codecs.open(os.path.join(self._output_dir, "Users_" + username + "_Quarantine_Events.txt"), "a", encoding="utf-8") as output_file:
+            output_file.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
             # if self._os_version in ["big_sur", "catalina", "mojave", "high_sierra", "sierra", "el_capitan", "yosemite",
             if self._os_version in ["catalina", "mojave", "high_sierra", "sierra", "el_capitan", "yosemite",
                                     "mavericks", "mountain_lion", "lion"]:
@@ -67,48 +66,7 @@ class UsersQuarantineEventsV2(Plugin):
                         "LSQuarantineSenderAddress,LSQuarantineTypeNumber,LSQuarantineOriginTitle," \
                         "LSQuarantineOriginURLString,LSQuarantineOriginAlias FROM LSQuarantineEvent"
                 if os.path.isfile(file):
-                    of.write("Source File: {0}\r\n\r\n".format(file))
-                    conn = None
-                    try:
-                        conn = sqlite3.connect(file)
-                        conn.row_factory = sqlite3.Row
-                        with conn:    
-                            cur = conn.cursor()
-                            cur.execute(query)
-                            rows = cur.fetchall()
-                            for row in rows:
-                                timestamp = riplib.osxripper_time.get_cocoa_seconds(row["LSQuarantineTimeStamp"])
-                                of.write("Event Identifier      : {0}\r\n".format(row["LSQuarantineEventIdentifier"]))
-                                of.write("Timestamp             : {0}\r\n".format(timestamp))
-                                of.write("AgentBundle Identifier: {0}\r\n"
-                                         .format(row["LSQuarantineAgentBundleIdentifier"]))
-                                of.write("Agent Name            : {0}\r\n".format(row["LSQuarantineAgentName"]))
-                                of.write("Data URL String       : {0}\r\n".format(row["LSQuarantineDataURLString"]))
-                                of.write("Sender Name           : {0}\r\n".format(row["LSQuarantineSenderName"]))
-                                of.write("Sender Address        : {0}\r\n".format(row["LSQuarantineSenderAddress"]))
-                                of.write("Type Number           : {0}\r\n".format(row["LSQuarantineTypeNumber"]))
-                                of.write("Origin Title          : {0}\r\n".format(row["LSQuarantineOriginTitle"]))
-                                of.write("Origin URL String     : {0}\r\n".format(row["LSQuarantineOriginURLString"]))
-                                of.write("Origin Alias          : {0}\r\n".format(row["LSQuarantineOriginAlias"]))
-                                of.write("\r\n")
-                            
-                    except sqlite3.Error as e:
-                        logging.error("{0}".format(e.args[0]))
-                        print("[ERROR] {0}".format(e.args[0]))
-                    finally:
-                        if conn:
-                            conn.close()
-                else:
-                    logging.warning("File: {0} does not exist or cannot be found.\r\n".format(file))
-                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
-                    print("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
-            elif self._os_version == "snow_leopard":
-                query = "SELECT LSQuarantineEventIdentifier,LSQuarantineTimeStamp,LSQuarantineAgentBundleIdentifier," \
-                        "LSQuarantineAgentName,LSQuarantineDataURLString,LSQuarantineSenderName," \
-                        "LSQuarantineSenderAddress,LSQuarantineTypeNumber,LSQuarantineOriginTitle," \
-                        "LSQuarantineOriginURLString,LSQuarantineOriginAlias FROM LSQuarantineEvent"
-                if os.path.isfile(file):
-                    of.write("Source File: {0}\r\n\r\n".format(file))
+                    output_file.write("Source File: {0}\r\n\r\n".format(file))
                     conn = None
                     try:
                         conn = sqlite3.connect(file)
@@ -119,32 +77,71 @@ class UsersQuarantineEventsV2(Plugin):
                             rows = cur.fetchall()
                             for row in rows:
                                 timestamp = riplib.osxripper_time.get_cocoa_seconds(row["LSQuarantineTimeStamp"])
-                                of.write("Event Identifier      : {0}\r\n".format(row["LSQuarantineEventIdentifier"]))
-                                of.write("Timestamp             : {0}\r\n".format(timestamp))
-                                of.write("AgentBundle Identifier: {0}\r\n"
-                                         .format(row["LSQuarantineAgentBundleIdentifier"]))
-                                of.write("Agent Name            : {0}\r\n".format(row["LSQuarantineAgentName"]))
-                                of.write("Data URL String       : {0}\r\n".format(row["LSQuarantineDataURLString"]))
-                                of.write("Sender Name           : {0}\r\n".format(row["LSQuarantineSenderName"]))
-                                of.write("Sender Address        : {0}\r\n".format(row["LSQuarantineSenderAddress"]))
-                                of.write("Type Number           : {0}\r\n".format(row["LSQuarantineTypeNumber"]))
-                                of.write("Origin Title          : {0}\r\n".format(row["LSQuarantineOriginTitle"]))
-                                of.write("Origin URL String     : {0}\r\n".format(row["LSQuarantineOriginURLString"]))
-                                of.write("Origin Alias          : {0}\r\n".format(row["LSQuarantineOriginAlias"]))
-                                of.write("\r\n")
+                                output_file.write("Event Identifier      : {0}\r\n".format(row["LSQuarantineEventIdentifier"]))
+                                output_file.write("Timestamp             : {0}\r\n".format(timestamp))
+                                output_file.write("AgentBundle Identifier: {0}\r\n".format(row["LSQuarantineAgentBundleIdentifier"]))
+                                output_file.write("Agent Name            : {0}\r\n".format(row["LSQuarantineAgentName"]))
+                                output_file.write("Data URL String       : {0}\r\n".format(row["LSQuarantineDataURLString"]))
+                                output_file.write("Sender Name           : {0}\r\n".format(row["LSQuarantineSenderName"]))
+                                output_file.write("Sender Address        : {0}\r\n".format(row["LSQuarantineSenderAddress"]))
+                                output_file.write("Type Number           : {0}\r\n".format(row["LSQuarantineTypeNumber"]))
+                                output_file.write("Origin Title          : {0}\r\n".format(row["LSQuarantineOriginTitle"]))
+                                output_file.write("Origin URL String     : {0}\r\n".format(row["LSQuarantineOriginURLString"]))
+                                output_file.write("Origin Alias          : {0}\r\n".format(row["LSQuarantineOriginAlias"]))
+                                output_file.write("\r\n")
 
-                    except sqlite3.Error as e:
-                        logging.error("{0}".format(e.args[0]))
-                        print("[ERROR] {0}".format(e.args[0]))
+                    except sqlite3.Error as error:
+                        logging.error("%s", error.args[0])
+                        print("[ERROR] {0}".format(error.args[0]))
                     finally:
                         if conn:
                             conn.close()
                 else:
-                    logging.warning("File: {0} does not exist or cannot be found.\r\n".format(file))
-                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+                    logging.warning("File: %s does not exist or cannot be found.\r\n", file)
+                    output_file.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+                    print("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+            elif self._os_version == "snow_leopard":
+                query = "SELECT LSQuarantineEventIdentifier,LSQuarantineTimeStamp,LSQuarantineAgentBundleIdentifier," \
+                        "LSQuarantineAgentName,LSQuarantineDataURLString,LSQuarantineSenderName," \
+                        "LSQuarantineSenderAddress,LSQuarantineTypeNumber,LSQuarantineOriginTitle," \
+                        "LSQuarantineOriginURLString,LSQuarantineOriginAlias FROM LSQuarantineEvent"
+                if os.path.isfile(file):
+                    output_file.write("Source File: {0}\r\n\r\n".format(file))
+                    conn = None
+                    try:
+                        conn = sqlite3.connect(file)
+                        conn.row_factory = sqlite3.Row
+                        with conn:
+                            cur = conn.cursor()
+                            cur.execute(query)
+                            rows = cur.fetchall()
+                            for row in rows:
+                                timestamp = riplib.osxripper_time.get_cocoa_seconds(row["LSQuarantineTimeStamp"])
+                                output_file.write("Event Identifier      : {0}\r\n".format(row["LSQuarantineEventIdentifier"]))
+                                output_file.write("Timestamp             : {0}\r\n".format(timestamp))
+                                output_file.write("AgentBundle Identifier: {0}\r\n".format(row["LSQuarantineAgentBundleIdentifier"]))
+                                output_file.write("Agent Name            : {0}\r\n".format(row["LSQuarantineAgentName"]))
+                                output_file.write("Data URL String       : {0}\r\n".format(row["LSQuarantineDataURLString"]))
+                                output_file.write("Sender Name           : {0}\r\n".format(row["LSQuarantineSenderName"]))
+                                output_file.write("Sender Address        : {0}\r\n".format(row["LSQuarantineSenderAddress"]))
+                                output_file.write("Type Number           : {0}\r\n".format(row["LSQuarantineTypeNumber"]))
+                                output_file.write("Origin Title          : {0}\r\n".format(row["LSQuarantineOriginTitle"]))
+                                output_file.write("Origin URL String     : {0}\r\n".format(row["LSQuarantineOriginURLString"]))
+                                output_file.write("Origin Alias          : {0}\r\n".format(row["LSQuarantineOriginAlias"]))
+                                output_file.write("\r\n")
+
+                    except sqlite3.Error as error:
+                        logging.error("%s", error.args[0])
+                        print("[ERROR] {0}".format(error.args[0]))
+                    finally:
+                        if conn:
+                            conn.close()
+                else:
+                    logging.warning("File: %s does not exist or cannot be found.\r\n", file)
+                    output_file.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
                     print("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
             else:
                 logging.warning("Not a known OSX version.")
                 print("[WARNING] Not a known OSX version.")
-            of.write("="*40 + "\r\n\r\n")
-        of.close()
+            output_file.write("="*40 + "\r\n\r\n")
+        output_file.close()
